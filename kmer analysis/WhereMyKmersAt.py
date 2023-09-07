0aba5333a1bfdc24a0ab6e6ce998865a9add737d
celneudorf <131936869+celneudorf@users.noreply.github.com>
2023-09-07 15:37:01 -0700

    Update WhereMyKmersAt.py

--- kmer analysis/WhereMyKmersAt.py
+++ kmer analysis/WhereMyKmersAt.py
@@ -14,44 +14,44 @@ with open(sys.argv[2]) as f:
 
 stepsize = 10000
 posDict={}
-for name,seq,q in mappy.fastx_read(sys.argv[1]):
+for name,seq,q in mappy.fastx_read(sys.argv[1]): #opening fasta file
     if name not in posDict:
-        posDict[name]={}
+        posDict[name]={}                         #creating dictionary of positions 
     print(name)
-    for index in range(0,len(seq),stepsize):
+    for index in range(0,len(seq),stepsize):     #iterating through step size 
         print(name,index)
         GenomekmerSet=set()
-        posDict[name][index]=[0,0]
-        subSeq=seq[index:index+stepsize]
-        for subIndex in range(0,len(subSeq),1):
-            kmer=subSeq[subIndex:subIndex+25]
-            GenomekmerSet.add(kmer)
-            if kmer in kmerSet:
-                posDict[name][index][0]+=1
+        posDict[name][index]=[0,0]               
+        subSeq=seq[index:index+stepsize] 
+        for subIndex in range(0,len(subSeq),1):  
+            kmer=subSeq[subIndex:subIndex+25]     
+            GenomekmerSet.add(kmer)              #adding 25 bp kmer to kmer set
+            if kmer in kmerSet:                  #counting how many times 25 bp kmer in window
+                posDict[name][index][0]+=1       
         out=open('kmers.fasta','w')
         for kmer in GenomekmerSet:
-            out.write(f'>1\n{kmer}\n')
+            out.write(f'>1\n{kmer}\n')           #writing out fasta file with kmer and count
         out.close()
         
-        os.system(f"jellyfish query {ActualGenomeKmers} -s kmers.fasta > query_output.tab") 
+        os.system(f"jellyfish query {ActualGenomeKmers} -s kmers.fasta > query_output.tab") #querying unique kmers 
         print('finished jellyfish')
         for line in open("query_output.tab"):
             GenomeLine=line.strip().split(' ')
             count=int(GenomeLine[1])
             if count==1:
-                posDict[name][index][1]+=1
-        if posDict[name][index][1]>0:
+                posDict[name][index][1]+=1       #counting how many times unique kmer in genome
+        if posDict[name][index][1]>0:           
             print(posDict[name][index][0],posDict[name][index][1],posDict[name][index][0]/posDict[name][index][1])
             
 counter=0
-out=open(sys.argv[3],'w')
-out.write('track type=wiggle_0\n')
+out=open(sys.argv[3],'w')                        #writing out wigfile 
+out.write('track type=wiggle_0\n') 
 for name in posDict:
     out.write(f'fixedStep chrom={name} start=1 step={stepsize} span={stepsize}\n')
-    for index in posDict[name]:
+    for index in posDict[name]:                  
         counter+=1
-        if posDict[name][index][1]>0:
-            out.write(str(posDict[name][index][0]/posDict[name][index][1])+'\n')
+        if posDict[name][index][1]>0:           #if kmer in reference genome
+            out.write(str(posDict[name][index][0]/posDict[name][index][1])+'\n') 
         else:
             out.write('0\n')
 out.close()
